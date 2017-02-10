7e9cf98c0411e0f56edf742dd6f71de18a958e74
jdw <jdw@NearlyFreeSpeech.NET>
2017-02-10 00:58:03 +0000

    Use central dehydrated.

--- nfsn-init.sh
+++ nfsn-init.sh
@@ -4,15 +4,11 @@ set -o errexit -o nounset -o pipefail
 readonly well_known='.well-known/acme-challenge/'
 declare single_cert='true'
 
-echo " + Cloning dehydrated git repository..."
-git submodule init
-git submodule update --remote
-
 echo " + Generating configuration..."
 for site_root in $(nfsn list-aliases); do
    if [[ -d "${DOCUMENT_ROOT}${site_root}/" ]]; then
       WELLKNOWN="${DOCUMENT_ROOT}${site_root}/${well_known}"
-      CONFIGDIR="dehydrated/certs/${site_root}/"
+      CONFIGDIR="certs/${site_root}/"
       mkdir -p "${WELLKNOWN}" "${CONFIGDIR}"
       echo "WELLKNOWN='${WELLKNOWN}'" > "${CONFIGDIR}/config"
       echo " + Installing hook script..."
@@ -24,11 +20,11 @@ done
 if [[ "${single_cert:+true}" ]]; then
    echo " + Generating fallback configuration..."
    mkdir -p "${DOCUMENT_ROOT}${well_known}"
-   echo "WELLKNOWN='${DOCUMENT_ROOT}${well_known}'" > dehydrated/config
+   echo "WELLKNOWN='${DOCUMENT_ROOT}${well_known}'" > config
    echo " + Installing hook script..."
-   echo "HOOK='$(realpath nfsn-hook.sh)'" >> dehydrated/config
+   echo "HOOK='$(realpath nfsn-hook.sh)'" >> config
 fi
 
 echo " + Generating domains.txt..."
-nfsn ${single_cert:+-s} list-aliases > dehydrated/domains.txt
+nfsn ${single_cert:+-s} list-aliases > domains.txt
 
