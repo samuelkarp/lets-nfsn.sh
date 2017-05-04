84d3f51d7cba9165c312bb9bcc5451d869246d4a
jdw <jdw@NearlyFreeSpeech.NET>
2017-05-04 18:39:56 +0000

    Remove scripts no longer used.

--- nfsn-renew.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-Dir=`dirname $0`
-RealDir=`realpath ${Dir}`
-exec ${RealDir}/dehydrated/dehydrated --cron
--- nfsn-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo
-echo Please transition to tls-setup.sh.
-echo
-echo See:
-echo https://members.nearlyfreespeech.net/forums/viewtopic.php?t=9230
-echo
-
